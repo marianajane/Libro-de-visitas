678a50ab45f5ca257d7e8229c7bb19d18636309b
marianajane <mariana.musm@gmail.com>
2017-03-26 00:46:58 -0600

    Ejercicio en proceso, sólo operan tres botones

---
+++ README.md
@@ -0,0 +1,7 @@
+# Libro-de-visitas
+Los archivos que contiene este ejercicio son
+###visitas.html
+###javascript.js
+###main.css
+
+Es un libro de visitas donde se puede agregar un comentario y a través de los botones ser modificado
---
+++ javascript.js
@@ -0,0 +1,71 @@
+function agregarComentario() {
+    var comentario= document.getElementById("articulo").value;
+    var loQueSea = comentario;
+    document.getElementById("aquiQueda").innerHTML = loQueSea;
+  }
+function escribir(){
+	var comentario= document.getElementById("articulo").value;
+    var loQueSea = comentario;
+    document.getElementById("aqui").innerHTML = loQueSea;
+}
+
+//Las siguientes tres funciones nos permiten cambiar el tamaño del texto
+var min=8;
+var max=30;
+function increaseFontSize() {
+
+   var p = document.getElementsByTagName('h6');
+   for(i=0;i<p.length;i++) {
+
+      if(p[i].style.fontSize) {
+         var s = parseInt(p[i].style.fontSize.replace("px",""));
+      } else {
+
+         var s = 30;
+      }
+      if(s!=max) {
+
+         s += 1;
+      }
+      p[i].style.fontSize = s+"px"
+
+   }
+}
+function decreaseFontSize() {
+   var p = document.getElementsByTagName('h6');
+   for(i=0;i<p.length;i++) {
+
+      if(p[i].style.fontSize) {
+         var s = parseInt(p[i].style.fontSize.replace("px",""));
+      } else {
+
+         var s = 14;
+      }
+      if(s!=min) {
+
+         s -= 1;
+      }
+      p[i].style.fontSize = s+"px"
+
+   }
+}
+
+function mediumFontSize(){
+	alert("Dont touch me yet!");
+}
+
+function cambiarColor(){
+	alert("Dont touch me yet!");
+}
+
+function alinearLeft(){
+	alert("Dont touch me yet!");
+}
+
+function alinearCenter(){
+	alert("Dont touch me yet!");
+}
+
+function alinearRight(){
+	alert("Dont touch me yet!");
+}
